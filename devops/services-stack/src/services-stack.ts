cfe9f7fabeeb104f272809b9e2b768fc8d55baa9
kevinand11 <kevinfizu@gmail.com>
2025-03-05 10:05:19 +0100

    services stack

--- devops/services-stack/src/services-stack.ts
+++ devops/services-stack/src/services-stack.ts
@@ -1,8 +1,139 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+/* import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets'; */
 
-export class ServicesStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+interface ServicesStackProps extends cdk.StackProps {
+  // domainName: string;
+  sshKeyName: string;
+}
+
+export class ServicesStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ServicesStackProps) {
     super(scope, id, props);
+
+    const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
+      isDefault: true
+    });
+
+    const securityGroup = new ec2.SecurityGroup(this, 'ServiceSecurityGroup', {
+      vpc,
+      description: 'Security group for services deployment',
+      allowAllOutbound: true
+    });
+
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allow SSH access'
+    );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'Allow HTTP traffic'
+    );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'Allow HTTPS traffic'
+    );
+
+    const instanceRole = new iam.Role(this, 'ServiceInstanceRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLogsFullAccess')
+      ]
+    });
+
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(
+      // System Update
+      'dnf update -y',
+      'dnf install -y docker docker-compose-plugin git nodejs npm',
+
+      // Enable Docker
+      'systemctl enable docker',
+      'systemctl start docker',
+
+      // Create Services Directory
+      'mkdir -p /opt/services/management',
+      'mkdir -p /opt/services/configs',
+      'mkdir -p /opt/services/scripts',
+
+      // Clone Management Service
+      'git clone https://github.com/your-org/management-service.git /opt/services/management',
+
+      // Setup Management Service
+      'cd /opt/services/management',
+      'npm install',
+
+      // Create Restart Script
+      'cat > /opt/services/scripts/restart-management-service.sh << EOL',
+      '#!/bin/bash',
+      'set -e',
+      'systemctl stop services-management || true',
+      'cd /opt/services/management',
+      'git pull origin main',
+      'npm install',
+      'systemctl start services-management',
+      'EOL',
+
+      // Make Scripts Executable
+      'chmod +x /opt/services/scripts/restart-management-service.sh',
+
+      // Create Systemd Service
+      'cat > /etc/systemd/system/services-management.service << EOL',
+      '[Unit]',
+      'Description=Services Management Service',
+      'After=docker.service',
+      'Requires=docker.service',
+      '[Service]',
+      'Type=simple',
+      'WorkingDirectory=/opt/services/management',
+      'ExecStart=/usr/bin/node index.js',
+      'Restart=always',
+      'User=ec2-user',
+      '[Install]',
+      'WantedBy=multi-user.target',
+      'EOL',
+
+      // Reload and Start Services
+      'systemctl daemon-reload',
+      'systemctl enable services-management',
+      'systemctl start services-management'
+    );
+
+    const instance = new ec2.Instance(this, 'ServiceInstance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      securityGroup: securityGroup,
+      role: instanceRole,
+      keyName: props.sshKeyName,
+      userData: userData
+    });
+
+    /* const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+      domainName: props.domainName
+    });
+
+    new route53.ARecord(this, 'ServiceDNSRecord', {
+      zone: hostedZone,
+      recordName: `services.${props.domainName}`,
+      target: route53.RecordTarget.fromIpAddresses(instance.instancePublicIp)
+    }); */
+
+    new cdk.CfnOutput(this, 'ServiceInstanceId', {
+      value: instance.instanceId,
+      description: 'Instance ID of the services host'
+    });
+
+    new cdk.CfnOutput(this, 'ServicePublicIP', {
+      value: instance.instancePublicIp,
+      description: 'Public IP of the services host'
+    });
   }
 }
