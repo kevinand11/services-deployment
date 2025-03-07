c569b6a02a2d8c325d1d7254b5a999ec686f4603
kevinand11 <kevinfizu@gmail.com>
2025-03-07 15:09:28 +0100

    fix

--- apps/management-service/src/index.ts
+++ apps/management-service/src/index.ts
@@ -96,7 +96,7 @@ app.post<{ Body: ServiceConfig & { name: string } }>(`/services`, async (req, re
       `traefik.http.middlewares.${name}-redirect-to-https.redirectscheme.scheme=https`,
       //`traefik.http.middlewares.${name}.stripprefix.prefixes=${config.domain}`,
       `traefik.http.routers.${name}.rule=Host(\`${config.domain}\`)`,
-      `traefik.http.routers.whoami.middlewares=${name}-redirect-to-https`,
+      `traefik.http.routers.${name}.middlewares=${name}-redirect-to-https`,
       //`traefik.http.routers.${name}.middlewares=${name}`,
       `traefik.http.routers.${name}.entrypoints=websecure`,
       `traefik.http.routers.${name}.tls=true`,
